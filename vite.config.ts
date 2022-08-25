c4a64b78466f0837c022fd1cf1a7c8cf40f3553f
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-08-25 21:58:19 +0200

    chore: change vite build format to iife

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,7 @@
-import type { OutputAsset, OutputChunk } from "rollup";
-import { defineConfig, PluginOption } from "vite";
-
-const isOutputChunk = (
-  chunkOrAsset: OutputChunk | OutputAsset,
-): chunkOrAsset is OutputChunk => {
-  return chunkOrAsset["code"] != null;
-};
-
-const wrapIIFE = (): PluginOption => ({
-  name: "wrap-iife",
-  generateBundle(options, bundle) {
-    const chunks = Object.values(bundle);
-
-    for (let i = 0; i < chunks.length; i += 1) {
-      const chunk = chunks[i];
-      if (isOutputChunk(chunk)) {
-        // eslint-disable-next-line no-param-reassign
-        chunk.code = `(function(){\n${chunk.code}\n})()`;
-      }
-    }
-  },
-});
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [wrapIIFE()],
   build: {
     minify: "esbuild",
 
